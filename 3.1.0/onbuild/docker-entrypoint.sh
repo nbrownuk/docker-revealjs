5cbbacabd1941e47fd2d32a0dbed1cc0ce46654d
Nigel Brown <nigel@windsock.io>
2015-11-04 09:38:44 +0000

    Initial commit

---
+++ 3.1.0/onbuild/docker-entrypoint.sh
@@ -0,0 +1,269 @@
+#!/bin/bash
+
+set -e
+
+file="index.html"
+
+usage () {
+cat <<EOF
+
+Usage: entrypoint.sh [OPTIONS]
+
+Options:
+
+--autoSlide=0                     Number of milliseconds before proceeding to
+                                  next slide
+--autoSlideStoppable=true         Stop auto-sliding after user input
+--backgroundTransition='default'  Transition style for full page slide
+                                  backgrounds*
+--center=true                     Vertical centering of slides
+--controls=true                   Display controls in the bottom right corner
+--embedded=false                  Flags if the presentation is running in
+                                  embedded mode, i.e. contained within a
+                                  limited portion of the screen
+--fragments=true                  Turns fragments on and off globally
+-h|--help                         Prints this usage text
+--hideAddressBar=true             Hides the address bar on mobile devices
+--history=false                   Push each slide change to the browser history
+--keyboard=true                   Enable keyboard shortcuts for navigation
+--loop=false                      Loop the presentation
+--mouseWheel=false                Enable slide navigation via mouse wheel
+--overview=true                   Enable the slide overview mode
+--parallaxBackgroundHorizontal=0  On slide change, amount of pixels to move
+                                  parallax background (horizontal)
+--parallaxBackgroundImage=''      Parallax background image location
+--parallaxBackgroundSize=''       Parallax background size in CSS syntax (only
+                                  pixel support, e.g. "2100px 900px)
+--parallaxBackgroundVertical=0    On slide change, amount of pixels to move
+                                  parallax background (vertical)
+--previewLinks=false              Open links in an iframe preview overlay
+--progress=true                   Displays a progress bar at bottom of screen
+--question=true                   Show help overlay when questionmark pressed
+--rtl=false                       Change presentation direction right to left
+--showNotes=false                 Sets speaker's notes visible to visible
+--slideNumber=false|''            Displays number of current slide; turn off
+                                  with false, default with true, 'c', 'c/t',
+                                  'h/v' (current, total, vertical, horizontal)
+--theme=''                        Specify an alternative theme from one of
+                                  those built in**
+--touch=true                      Enables touch navigation on devices that
+                                  support it
+--transition='default'            Slide transition style*
+--transitionSpeed='default'       Transition speed (default|slow|fast)
+--viewDistance=3                  Number of slides away from the current that
+                                  are pre-loaded
+
+          *  Transition styles: default|none|fade|slide|convex|concave|zoom
+          ** Themes: black|white|league|beige|sky|night|serif|simple|solarized
+
+EOF
+}
+
+
+valid_arg () {
+    case "$1" in
+        --autoSlide|--viewDistance|--parallaxBackgroundHorizontal|--parallaxBackgroundVertical)
+            if [[ "$2" =~ ^[0-9]+$ ]]; then
+                return
+            else
+                echo "$1: bad argument, please specify an integer"
+            fi
+            ;;
+        --parallaxBackgroundImage)
+            if wget -S --spider "$2" 2>&1 | grep -q "HTTP/1.1 200 OK"; then
+                return
+            else
+                echo "$1: bad argument, please specify a valid URL to file"
+            fi
+            ;;
+        --parallaxBackgroundSize)
+            if echo "$2" | grep -qE "^[[:digit:]]*px [[:digit:]]*px"; then
+                return
+            else
+                echo "$1: bad argument, please specify size in px (e.g. 2100px 900px)"
+            fi
+            ;;
+        --slideNumber)
+            case "$2" in
+                true|false|c|h/v|c/t)
+                    return
+                    ;;
+                *)
+                    echo "$1: bad argument, please specify true, false, c, c/t or h/v"
+                    ;;
+            esac
+            ;;
+        --theme)
+            case "$2" in
+                black|white|league|beige|sky|night|serif|simple|solarized)
+                    return
+                    ;;
+                *)
+                    echo "$1: bad argument, please specify one of black, white, league, beige, sky, night, serif, simple, solarized"
+                    ;;
+            esac
+            ;;
+        --transition|--backgroundTransition)
+            case "$2" in
+                default|none|fade|slide|convex|concave|zoom)
+                    return
+                    ;;
+                *)
+                    echo "$1: bad argument, please specify one of default, none, fade, slide, convex, concave or zoom"
+                    ;;
+            esac
+            ;;
+        --transitionSpeed)
+            case "$2" in
+                default|slow|fast)
+                    return
+                    ;;
+                *)
+                    echo "$1: bad argument, please specify one of default, slow or fast"
+                    ;;
+            esac
+            ;;
+        *)
+            case "$2" in
+                true|false)
+                    return
+                    ;;
+                *)
+                    echo "$1: bad argument, please specify true or false"
+                    ;;
+            esac
+            ;;
+    esac
+    exit 1
+}
+
+
+set_config () {
+    set -- "${1#--}" "${@:2}"
+    if [ "$1" = 'theme' ]; then
+        sed -ri "s/(^[[:blank:]]*<link rel=\"stylesheet\" href=\"css\/$1\/)[[:alnum:]]+(\.css\")/\1$2\2/" "$file"
+    else
+        set -- "${1/question/help}" "${@:2}"
+        case $(grep -E "^[[:blank:]]*$1:[[:blank:]]+'{0,1}[^']+'{0,1}.*" "$file" | wc -l) in
+            0)
+                insert_config_param $1 $2
+                ;;
+            1)
+                amend_config_param $1 $2
+                ;;
+            *)
+                delete_config_param $1 $2
+                insert_config_param $1 $2
+                ;;
+        esac
+    fi
+}
+
+
+delete_config_param () {
+    sed -ri "/^[[:blank:]]*$1:[[:blank:]]+'{0,1}[^']+'{0,1}.*/d" "$file"
+}
+
+
+amend_config_param () {
+    if [ "$1" = 'slideNumber' ]; then
+        case "$2" in
+            c|h/v|c/t)
+                if grep -qE "^[[:blank:]]*$1:[[:blank:]]+[[:alpha:]]+.*," "$file"; then
+                    set -- "$1" "${2/$2/\'$2\'}"
+                fi
+                ;;
+            *)
+                if grep -qE "^[[:blank:]]*$1:[[:blank:]]+'[^']+',.*" "$file"; then
+                    sed -ri "s/(^[[:blank:]]*$1:[[:blank:]]*)'([^']+)'(,).*/\1\2\3/" "$file"
+                fi
+                ;;
+        esac
+    fi
+    sed -ri "s|(^[[:blank:]]*$1:[[:blank:]]*'{0,1})[^']+('{0,1},).*|\1$2\2|" "$file"
+}
+
+
+insert_config_param () {
+    WSPACE=$(sed -rn 's/^([[:blank:]]*)Reveal.initialize\(\{/\1/p' "$file")
+    case "$1" in
+        transition|transitionSpeed|backgroundTransition|parallaxBackgroundImage|parallaxBackgroundSize)
+            sed -ri "/^[[:blank:]]*Reveal.initialize\(\{/ a\\$WSPACE\t$1: '$2'," "$file"
+            ;;
+        slideNumber)
+            case "$2" in
+                true|false)
+                    sed -ri "/^[[:blank:]]*Reveal.initialize\(\{/ a\\$WSPACE\t$1: $2," "$file"
+                    ;;
+                *)
+                    sed -ri "/^[[:blank:]]*Reveal.initialize\(\{/ a\\$WSPACE\t$1: '$2'," "$file"
+                    ;;
+            esac
+            ;;
+        *)
+            sed -ri "/^[[:blank:]]*Reveal.initialize\(\{/ a\\$WSPACE\t$1: $2," "$file"
+            ;;
+    esac
+}
+
+
+OPTIONS=$(getopt -n "$0" \
+            -o "h" \
+            -l "autoSlide:, \
+                autoSlideStoppable:, \
+                backgroundTransition:, \
+                center:, \
+                controls:, \
+                embedded:, \
+                fragments:, \
+                help, \
+                hideAddressBar:, \
+                history:, \
+                keyboard:, \
+                loop:, \
+                mouseWheel:, \
+                overview:, \
+                parallaxBackgroundHorizontal:, \
+                parallaxBackgroundImage:, \
+                parallaxBackgroundSize:, \
+                parallaxBackgroundVertical:, \
+                previewLinks:, \
+                progress:, \
+                question:, \
+                rtl:, \
+                showNotes:, \
+                slideNumber:, \
+                theme:, \
+                touch:, \
+                transition:, \
+                transitionSpeed:, \
+                viewDistance:" \
+            -- \
+            "$@")
+
+eval set -- "$OPTIONS"
+
+while true; do
+    case "$1" in
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            valid_arg "$1" "$2"
+            set_config "$1" "$2"
+            shift 2
+            ;;
+    esac
+done
+
+
+if [ $# -eq 0 ]; then
+    set -- "grunt" "serve"
+fi
+
+exec "$@"
---
+++ README.md
@@ -0,0 +1,98 @@
+# Tags and respective `Dockerfile` links
+
+- [`3.1.0-onbuild`, `3-onbuild`, `onbuild` (3.1.0/onbuild/Dockerfile)](./3.1.0/onbuild/Dockerfile)
+- [`3.1.0`, `3`, `latest` (3.1.0/Dockerfile)](./3.1.0/Dockerfile)
+
+# What is reveal.js?
+
+[Reveal.js](https://github.com/hakimel/reveal.js) is a framework for creating [slick HTML-based presentations](https://github.com/hakimel/reveal.js/wiki/Example-Presentations). Based on Node.js, reveal.js provides a highly customisable presentation experience, with slides being authored in HTML and/or Markdown.
+
+In addition to the choice of numerous themes shipped as standard with reveal.js, presentation behaviour and 'look and feel' can be further customised with [plugins](https://github.com/hakimel/reveal.js/wiki/Plugins,-Tools-and-Hardware) and runtime [configuration options](https://github.com/hakimel/reveal.js#configuration).
+
+# How to use this image
+
+## Encapsulating a presentation inside an image
+
+Once a reveal.js presentation has been successfully authored, it can be encapsulated inside a Docker image. This makes it easy to store and share presentations - presentations can be pulled from a registry as a Docker image, with its content baked inside the image.
+
+In order to encapsulate a presentation inside an image, use the `onbuild` variant. To build the image, create a `Dockerfile` with the single instruction:
+
+```
+FROM nbrown/revealjs:3.1.0-onbuild
+```
+
+and ensure the following are in the build context:
+
+```
+index.html    // the basis of the presentation
+md/           // a directory containing a file with slides in markdown format, if required
+media/        // a directory containing any media used in the presentation, if required
+```
+
+The media directory can contain video or image files for your presentation; just reference them relative to the current working directory; e.g.
+
+```
+<img src="media/UTS_Namespace.svg" alt="UTS Namespace" />
+```
+
+Additionally, the following directories are required in the build context for the inclusion of any customisations (see [https://github.com/hakimel/reveal.js#folder-structure](https://github.com/hakimel/reveal.js#folder-structure)):
+```
+css/
+js/
+plugin/
+lib/
+```
+
+Not all presentations will require content in all of the directories above. However, because Docker doesn't currently have a [conditional `COPY` or `ADD` `Dockerfile` instruction](https://github.com/docker/docker/issues/13045), all of the above MUST exist in the build context, even if the directories are empty, otherwise the build will fail.
+
+To build a Docker image encapsulating the presentation, and then run it; from the build context:
+
+```
+$ docker build -t my_presentation .
+$ docker run -it --rm -p 8000:8000 my_presentation
+```
+
+Once a presentation is baked inside a Docker image, seemingly its configuration is fixed. The `onbuild` image, however, contains an entrypoint script which allows for runtime configuration. For example, to override the configuration of a presentation that normally requires the presenter stepping through the slides, so that it 'auto presents', use the following:
+
+```
+$ docker run -it --rm -p 8000:8000 my_presentation --loop=true --autoSlide=5000 --autoSlideStoppable=true
+```
+
+The entrypoint script updates the `index.html` file at runtime to reflect the chosen configuration. To find out what configuration options are available at runtime, run:
+
+```
+$ docker run --rm my_presentation --help
+```
+
+There are a large number of customisations available!
+
+## Developing a presentation
+
+Developing an HTML-based presentation with reveal.js is an iterative process, and its important to review changes as you progress. It is much easier to do this without having to build a new Docker image each time you make a change. For developing presentations, use the standard image version, and mount your presentation content from the host into the container.
+
+If you run the standard image without any mounted content, you'll get the standard reveal.js demo presentation:
+
+```
+$ docker run -it --rm -p 8000:8000 nbrown/revealjs
+```
+
+If you have a presentation, with your content located in the current working directory, you might invoke the presentation using:
+
+```
+$ docker run -it --rm -p 8000:8000 -v $PWD/index.html:/reveal.js/index.html:ro \
+-v $PWD/media:/reveal.js/media:ro -v $PWD/custom.css:/reveal.js/css/theme/custom.css:ro \
+-v $PWD/menu:/reveal.js/plugin/menu:ro nbrown/revealjs
+```
+
+Changes that are made to the `index.html` file are watched for, and a server reload occurs when changes are saved. Hence, refreshing your browser will show the effects of any changes made to the `index.html` file, without a restart of the container.
+
+This image can also be used to create themes for reveal.js. Themes are authored [using Sass](https://github.com/hakimel/reveal.js/blob/master/css/theme/README.md#creating-a-theme). Once you've defined your theme in Sass, it can be compiled into CSS using the standard image. Create an empty file in your presentation directory for the compiled Sass, mount this into your container along with the Sass source file, and issue the `grunt css-themes` command to compile it into CSS:
+
+```
+$ > custom.css
+$ docker run -it --rm -v $PWD/custom.css:/reveal.js/css/themes/custom.css \
+-v $PWD/custom.scss:/reveal.js/css/themes/source/custom.scss nbrown/revealjs \
+grunt css-themes
+```
+
+The `custom.css` file will now contain your theme as CSS, and can be used in your presentation by mounting into your container at runtime.
